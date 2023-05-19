ba5349c772a5aa26b9c8109d524a75217477b6d7
sathv <sathv@DESKTOP-HROEPU7>
2023-05-19 12:02:55 +0530

    first commit

---
+++ springcore_module1/src/main/java/com/spr_core/SpringcoreModule1Application.java
@@ -0,0 +1,13 @@
+package com.spr_core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcoreModule1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcoreModule1Application.class, args);
+	}
+
+}
