2b637e86fbd875951a8e85296860c582ffd616cf
sathv <sathv@DESKTOP-HROEPU7>
2023-05-19 15:44:34 +0530

    first commit

---
+++ springcore_module1/src/test/java/com/spr_core/SpringcoreModule1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spr_core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcoreModule1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
